e865a77dd9b309d111c4855c8a5e949509e0e6a8
danielabonfim89 <daniela.bonfimoliveira@gmail.com>
2022-01-10 15:50:24 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormLoginComponent } from './form-login/form-login.component';
+import { FormsCadastroSocioComponent} from './forms-cadastro-socio/forms-cadastro-socio.component';
+import { FormCategoriaComponent } from './form-categoria/form-categoria.component';
+import { FormMensalidadeComponent } from './form-mensalidade/form-mensalidade.component';
+import { HomeClubeComponent } from './home-clube/home-clube.component';
+import { FormsCadastroDependenteComponent } from './forms-cadastro-dependente/forms-cadastro-dependente.component';
+
+const routes: Routes = [
+  { path: "login", component: FormLoginComponent},
+  { path: "socio", component: FormsCadastroSocioComponent},
+  { path: "dependente", component: FormsCadastroDependenteComponent},
+  { path: "categoria", component: FormCategoriaComponent},
+  { path: "mensalidade", component: FormMensalidadeComponent},
+  { path: "home", component: HomeClubeComponent}
+  //{ path: "", component: FormLoginComponent}
+
+];
+
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
